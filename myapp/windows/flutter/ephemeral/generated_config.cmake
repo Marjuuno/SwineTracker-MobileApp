9092c402411e91babcff6844c1b735f045e014d9
unknown <mmapa@gbox.adnu.edu.ph>
2025-05-07 20:24:30 +0800

    You successfully added myapp

---
+++ myapp/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Marjun\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Marjun\\Flutter Project\\myapp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Marjun\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\Marjun\\Flutter Project\\myapp"
+  "FLUTTER_ROOT=C:\\Users\\Marjun\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Marjun\\Flutter Project\\myapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Marjun\\Flutter Project\\myapp"
+  "FLUTTER_TARGET=C:\\Users\\Marjun\\Flutter Project\\myapp\\lib\\praktismain.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Marjun\\Flutter Project\\myapp\\.dart_tool\\package_config.json"
+)
